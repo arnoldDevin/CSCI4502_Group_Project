2fbd91e1f65e70336c97ef679b2005fe4061e9e5
matthewcoker <32140125+matthewcoker@users.noreply.github.com>
2018-04-09 22:47:32 -0600

    MattVis

--- MattsVis.py
+++ MattsVis.py
@@ -7,13 +7,15 @@ from bokeh.plotting import figure
 from bokeh.transform import factor_cmap
 
 o = open('yelp_business_attributes.csv')
-dogsAllowed = [] #column 33
+dogsAllowed = []
 alcohol = []
 smoking = []
 hasTV = []
 happyHour = []
 goodForKids = []
 
+#Open CSV, read in certain columns and append each
+#of their values to a list named after thei attribute
 csv_o = csv.reader(o)
 for row in csv_o:
 	dogsAllowed.append(str(row[67]))
@@ -21,27 +23,46 @@ for row in csv_o:
 	smoking.append(str(row[65]))
 	hasTV.append(str(row[27]))
 	happyHour.append(str(row[46]))
+	goodForKids.append(str(row[18]))
 o.close()
 
+#These variables are used to find the amount of 'Trues' or non-'Na'
 alc = 0
 smoke = 0
 TV = 0
 happy = 0
 kids = 0
+count = 0
 
 for value in range(len(dogsAllowed)):
-	if (dogsAllowed[value] == "TRUE" and alcohol[value] == "TRUE"):
-		alc += 1
-	if (dogsAllowed[value] == "TRUE" and smoking[value] == "TRUE"):
-		smoke += 1
-	if (dogsAllowed[value] == "TRUE" and hasTV[value] != "Na"):
-		TV += 1
-	if (dogsAllowed[value] == "TRUE" and happyHour[value] == "TRUE"):
-		happy += 1
-	if (dogsAllowed[value] == "TRUE" and goodForKids[value] != "Na"):
-		kids += 1
+	#There are NO places where these attributes are both true, this is ignored pretty much
+	#A lot of the attributes just have and entire column of Na's
+	if (dogsAllowed[value] == 'True' and alcohol[value] == 'True'):
+		alc = alc + 1
+	if (dogsAllowed[value] == 'True' and smoking[value] == 'True'):
+		smoke = smoke +  1
+	if (dogsAllowed[value] == 'True' and hasTV[value] != 'Na'):
+		TV = TV + 1
+	if (dogsAllowed[value] == 'True' and happyHour[value] == 'True'):
+		happy = happy + 1
+	if alcohol[value] == 'True' and goodForKids[value] != 'Na':
+		kids = kids + 1
+
+	#Counted up the amount of places that serves alcohol, allows smoking, has
+	#a TV, and is good for kids. Compared them all on a bar chart
+	if (alcohol[value] == 'True'):
+		alc = alc + 1
+	if (smoking[value] == 'True'):
+		smoke = smoke +  1
+	if (hasTV[value] != 'Na'):
+		TV = TV + 1
+	if (happyHour[value] == 'True'):
+		happy = happy + 1
+	if goodForKids[value] != 'Na':
+		kids = kids + 1
 
 
+#Bar Chart
 dogsAlloweds = ["Alcohol", "Smoking", "Has a TV", "Happy Hour", "Good for Kids"]
 
 p = figure(x_range=dogsAlloweds, plot_height=350, title="Ammount of Places That Allow Dogs as well as Other Things",
